f2379944fac6ca7d3412200139c9f3b4cc5e4a4e
tomeklarysz <ttomek.larysz@gmail.com>
2024-03-29 20:27:40 +0100

    main content first items

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,8 @@
+// import { createTask } from 
 import { projectsStorage } from "./projects";
 import './style.css';
 
 function dom() {
-  const container = document.getElementById('container');
   const sidebar = document.getElementById('sidebar');
   const content = document.getElementById('main-content');
   
@@ -19,6 +19,37 @@ function dom() {
       sidebar.appendChild(listDiv);
     }
   }
+  const h1 = document.createElement('h1');
+  h1.textContent = projectsStorage.storage[0].lists[0].getListTitle();
+  content.appendChild(h1);
+  const tasksList = document.createElement('ul');
+  content.appendChild(tasksList);
+  for (const task of projectsStorage.storage[0].lists[0].getTasks()) {
+    const item = document.createElement('li');
+    item.textContent = task.getTitle();
+    tasksList.appendChild(item);
+  }
+
+
+  const newTaskDiv = document.createElement('div');
+  const newTaskBtn = document.createElement('button');
+  newTaskBtn.textContent = 'new task';
+  newTaskDiv.appendChild(newTaskBtn);
+  newTaskBtn.addEventListener('click', () => {
+    newTaskDiv.removeChild(newTaskBtn);
+    const taskInput = document.createElement('input');
+    taskInput.setAttribute('type', 'text');
+    taskInput.setAttribute('id', 'task');
+    taskInput.setAttribute('name', 'task');
+    taskInput.setAttribute('placeholder', 'check mails..');
+    taskInput.addEventListener("keyup", (event) => {
+      if (event.key === "Enter") {
+
+      }
+    });
+    newTaskDiv.appendChild(taskInput);
+  });
+  content.appendChild(newTaskDiv);
 }
 
 export { dom };
