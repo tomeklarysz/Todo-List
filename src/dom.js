959e112cb29d5192a43c2de15fcaf1d8da2f545d
tomeklarysz <ttomek.larysz@gmail.com>
2024-03-30 13:11:33 +0100

    functions to remove all parent's children, eventListener for lists

--- src/dom.js
+++ src/dom.js
@@ -1,11 +1,16 @@
 import { projectsStorage } from "./projects";
 import './style.css';
 
-
 // dom as iife
 const dom = (function () {
   const content = document.getElementById('main-content');
   const sidebar = document.getElementById('sidebar');
+
+  function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
 
   function initialDisplayContent() {
     const h1 = document.createElement('h1');
@@ -18,6 +23,21 @@ const dom = (function () {
       item.textContent = task.getTitle();
       tasksList.appendChild(item);
     }
+  }
+
+  function displayContent(list) {
+    removeAllChildNodes(content);
+    const h1 = document.createElement('h1');
+    h1.textContent = list.getListTitle();
+    content.appendChild(h1);
+    const tasksList = document.createElement('ul');
+    content.appendChild(tasksList);
+    for (const task of list.getTasks()) {
+      const item = document.createElement('li');
+      item.textContent = task.getTitle();
+      tasksList.appendChild(item);
+    }
+    newTask();
   }
 
   function newTask() {
@@ -43,9 +63,7 @@ const dom = (function () {
   }
 
   function displaySidebar() {
-    /* TODO
-    remove sidebar children so when we 
-    add new projects it works properly */
+    removeAllChildNodes(sidebar);
     for (const project of projectsStorage.storage) {
       const projectDiv = document.createElement('div');
       projectDiv.classList.add('project');
@@ -56,6 +74,9 @@ const dom = (function () {
         const listDiv = document.createElement('div');
         listDiv.classList.add('list');
         listDiv.textContent = list.getListTitle();
+        listDiv.addEventListener('click', () => {
+          displayContent(list);
+        });
         sidebar.appendChild(listDiv);
       }
 
