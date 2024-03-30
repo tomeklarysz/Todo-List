49f987d9d5615444048ad8b937c085d0c10b0df3
tomeklarysz <ttomek.larysz@gmail.com>
2024-03-30 12:57:07 +0100

    dom as iife

--- src/dom.js
+++ src/dom.js
@@ -1,55 +1,68 @@
-// import { createTask } from 
 import { projectsStorage } from "./projects";
 import './style.css';
 
-function dom() {
-  const sidebar = document.getElementById('sidebar');
+
+// dom as iife
+const dom = (function () {
   const content = document.getElementById('main-content');
-  
-  for (const project of projectsStorage.storage) {
-    const projectDiv = document.createElement('div');
-    projectDiv.classList.add('project');
-    projectDiv.textContent = project.getProjectTitle();
-    projectDiv.style.marginTop = '15px';
-    sidebar.appendChild(projectDiv);
-    for (const list of project.lists) {
-      const listDiv = document.createElement('div');
-      listDiv.classList.add('list');
-      listDiv.textContent = list.getListTitle();
-      sidebar.appendChild(listDiv);
+  const sidebar = document.getElementById('sidebar');
+
+  function initialDisplayContent() {
+    const h1 = document.createElement('h1');
+    h1.textContent = projectsStorage.storage[0].lists[0].getListTitle();
+    content.appendChild(h1);
+    const tasksList = document.createElement('ul');
+    content.appendChild(tasksList);
+    for (const task of projectsStorage.storage[0].lists[0].getTasks()) {
+      const item = document.createElement('li');
+      item.textContent = task.getTitle();
+      tasksList.appendChild(item);
     }
   }
-  const h1 = document.createElement('h1');
-  h1.textContent = projectsStorage.storage[0].lists[0].getListTitle();
-  content.appendChild(h1);
-  const tasksList = document.createElement('ul');
-  content.appendChild(tasksList);
-  for (const task of projectsStorage.storage[0].lists[0].getTasks()) {
-    const item = document.createElement('li');
-    item.textContent = task.getTitle();
-    tasksList.appendChild(item);
+
+  function newTask() {
+    const newTaskDiv = document.createElement('div');
+    const newTaskBtn = document.createElement('button');
+    newTaskBtn.textContent = 'new task';
+    newTaskDiv.appendChild(newTaskBtn);
+    newTaskBtn.addEventListener('click', () => {
+      newTaskDiv.removeChild(newTaskBtn);
+      const taskInput = document.createElement('input');
+      taskInput.setAttribute('type', 'text');
+      taskInput.setAttribute('id', 'task');
+      taskInput.setAttribute('name', 'task');
+      taskInput.setAttribute('placeholder', 'check mails..');
+      taskInput.addEventListener("keyup", (event) => {
+        if (event.key === "Enter") {
+          /* finish this */
+        }
+      });
+      newTaskDiv.appendChild(taskInput);
+    });
+    content.appendChild(newTaskDiv);
   }
 
-
-  const newTaskDiv = document.createElement('div');
-  const newTaskBtn = document.createElement('button');
-  newTaskBtn.textContent = 'new task';
-  newTaskDiv.appendChild(newTaskBtn);
-  newTaskBtn.addEventListener('click', () => {
-    newTaskDiv.removeChild(newTaskBtn);
-    const taskInput = document.createElement('input');
-    taskInput.setAttribute('type', 'text');
-    taskInput.setAttribute('id', 'task');
-    taskInput.setAttribute('name', 'task');
-    taskInput.setAttribute('placeholder', 'check mails..');
-    taskInput.addEventListener("keyup", (event) => {
-      if (event.key === "Enter") {
-
+  function displaySidebar() {
+    /* TODO
+    remove sidebar children so when we 
+    add new projects it works properly */
+    for (const project of projectsStorage.storage) {
+      const projectDiv = document.createElement('div');
+      projectDiv.classList.add('project');
+      projectDiv.textContent = project.getProjectTitle();
+      projectDiv.style.marginTop = '15px';
+      sidebar.appendChild(projectDiv);
+      for (const list of project.lists) {
+        const listDiv = document.createElement('div');
+        listDiv.classList.add('list');
+        listDiv.textContent = list.getListTitle();
+        sidebar.appendChild(listDiv);
       }
-    });
-    newTaskDiv.appendChild(taskInput);
-  });
-  content.appendChild(newTaskDiv);
-}
+
+    }
+  }
+
+  return { initialDisplayContent, newTask, displaySidebar };
+})();
 
 export { dom };
